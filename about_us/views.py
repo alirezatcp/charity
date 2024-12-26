b522482feb919543fba1b8280434c557dc69859d
alirezatcp <alirezataaty02@gmail.com>
2024-12-26 14:52:47 -0500

    Add view for about_us app.

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from accounts.models import User
+
+@csrf_exempt
+def about_us(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        context = {'users': users}
+        return render(request=request, template_name='about_us.html', context=context)
+
+
